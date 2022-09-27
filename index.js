737021e31c2cef44c5d413d0930285151eccf69d
Roshel Esteron <mynameisroshel19@gmail.com>
2022-09-27 20:54:46 +1000

    commit tree b6e59e2d115534d6b5c84faa06c758e598f4c500

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(catName){
+    const newCatsArray = [...cats,catName];
+    //newCatsArray.push(catName);
+    return newCatsArray;
+}
+function prependCat(catName) {
+    const newCatsArray = [catName,...cats];
+    //newCatsArray.unshift(catName);
+    return newCatsArray;
+}
+function removeLastCat() {
+    const newCatsArray = [...cats];
+    newCatsArray.pop();
+    return newCatsArray;
+}
+function removeFirstCat() {
+    const newCatsArray = [...cats];
+    newCatsArray.shift();
+    return newCatsArray;
+}
